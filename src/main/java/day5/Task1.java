2948898be1564b1d54410a9d584a7204cd5c1c76
Gennady Kotsenya <genkoc21@gmail.com>
2022-03-07 18:57:36 +0300

    add day 5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -3,5 +3,46 @@ package day5;
 public class Task1 {
     public static void main(String[] args) {
 
+        Car tesla = new Car();
+
+        tesla.setModel("X");
+        tesla.setColor("White");
+        tesla.setYearOfRelease(2015);
+
+        System.out.println(tesla.getModel());
+        System.out.println(tesla.getColor());
+        System.out.println(tesla.getYearOfRelease());
     }
 }
+
+class Car {
+    private String model;
+    private String color;
+    private int yearOfRelease;
+
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public void setYearOfRelease(int yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+    }
+}
+
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -4,5 +4,36 @@ package day5;
 public class Task2 {
     public static void main(String[] args) {
 
+        Motorbike honda = new Motorbike("CBR650R", "Red", 2019);
+
+        System.out.println(honda.getModel());
+        System.out.println(honda.getColor());
+        System.out.println(honda.getYearOfRelease());
+    }
+}
+
+class Motorbike {
+
+    private String model;
+    private String color;
+    private int yearOfRelease;
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public Motorbike(String model, String color, int yearOfRelease) {
+        this.model = model;
+        this.color = color;
+        this.yearOfRelease = yearOfRelease;
+
     }
 }
