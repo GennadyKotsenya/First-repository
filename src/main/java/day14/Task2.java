859eb39ef1722c3e26b0835efb7d6ae313e93c4b
Gennady Kotsenya <genkoc21@gmail.com>
2022-03-17 13:03:19 +0300

    add day 14

--- src/main/java/day14/Task2.java
+++ src/main/java/day14/Task2.java
@@ -1,7 +1,41 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
 
+        System.out.println(parseFileToStringList(new File("people.txt")));
+
     }
+
+    public static List<String> parseFileToStringList(File file) {
+
+        List<String> peoples = new ArrayList<>();
+
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] partList = line.split(" ");
+
+                if (Integer.parseInt(partList[1]) < 0) {
+                    throw new IllegalArgumentException();
+                }
+                peoples.add(line);
+            }
+            return peoples;
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Некорректный входной файл");
+        }
+        return null;
+    }
+
 }
