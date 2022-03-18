84cc4c67ef453d69bd6c6b398ef01678c2716da5
Gennady Kotsenya <genkoc21@gmail.com>
2022-03-18 12:30:50 +0300

    add day 15

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,39 @@
 package day15;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args){
+
+        File file = new File("src/main/resources/shoes.csv");
+        File file2 = new File("src/main/resources/missing_shoes.txt");
+
+        try {
+            PrintWriter pw = new PrintWriter(file2);
+            Scanner scanner = new Scanner(file);
+
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] partList = line.split(";");
+
+                if (partList.length < 3)
+                    throw new IllegalArgumentException();
+
+                if (Integer.parseInt(partList[2]) == 0) {
+                    pw.println(line);
+                }
+            }
+            pw.close();
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Фйл не найден");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Некорректный входной файл");
+        }
     }
 }
+
