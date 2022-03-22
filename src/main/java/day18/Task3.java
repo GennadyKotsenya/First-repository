f82f2475c691bc957a7a786f26b89997fe9e1740
Gennady Kotsenya <genkoc21@gmail.com>
2022-03-22 12:22:38 +0300

    add day 18

--- src/main/java/day18/Task3.java
+++ src/main/java/day18/Task3.java
@@ -3,5 +3,46 @@ package day18;
 public class Task3 {
     public static void main(String[] args) {
 
+        Node root = new Node(20);
+        int[] numbers = {14, 23, 11, 16, 22, 27, 5, 15, 18, 24, 150, 8};
+        for (int number : numbers)
+            addNode(number, root);
+
+        dfs(root);
+    }
+
+    private static void dfs(Node node) {
+        if (node == null)
+            return;
+
+        dfs(node.getLeft());
+        System.out.print(node.getValue() + " ");
+        dfs(node.getRight());
+    }
+
+    private static void addNode(int insertValue, Node root) {
+
+        if (root == null)
+            throw new IllegalArgumentException("Корневой узел не должен быть null");
+
+
+        Node nextNode = root;
+        Node currentNode = null;
+
+        while (nextNode != null) {
+            currentNode = nextNode;
+
+            if (insertValue < currentNode.getValue()) {
+                nextNode = currentNode.getLeft();
+            } else {
+                nextNode = currentNode.getRight();
+            }
+        }
+
+        if (insertValue < currentNode.getValue()) {
+            currentNode.setLeft(new Node(insertValue));
+        } else {
+            currentNode.setRight(new Node(insertValue));
+        }
     }
 }
