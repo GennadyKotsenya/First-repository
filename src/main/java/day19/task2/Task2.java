e066d564252b02c755be668254a5c9dc305048c3
Gennady Kotsenya <genkoc21@gmail.com>
2022-04-13 09:39:44 +0300

    add task2 day19

--- src/main/java/day19/task2/Task2.java
+++ src/main/java/day19/task2/Task2.java
@@ -2,6 +2,7 @@ package day19.task2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.*;
 
 /**
@@ -9,6 +10,7 @@ import java.util.*;
  */
 public class Task2 {
 
+    static Scanner scanner2 = new Scanner(System.in);
 
     public static void main(String[] args) throws FileNotFoundException {
 
@@ -28,63 +30,62 @@ public class Task2 {
 
         }
 
-        Set<Point> setValuesInSquare = new HashSet<>();
-
-        inputValue(setValuesInSquare);
-
+        Set<Point> set = getValueRequiredField();
 
         int counter = 0;
         for (Map.Entry<Integer, Point> linesMap : mapFile.entrySet()) {
 
-            if (setValuesInSquare.contains(linesMap.getValue())) {
+            if (set.contains(linesMap.getValue())) {
                 counter++;
                 System.out.println(linesMap.getKey());
             }
         }
-        System.out.println(counter);
-        System.out.println(setValuesInSquare);
+        System.out.println("\nКоличество машин в квадрате: " + counter);
     }
 
+    public static Set<Point> getValueRequiredField() {
 
-    public static void inputValue(Set<Point> valuesInSquare) {
-        Scanner scanner2 = new Scanner(System.in);
-
-        System.out.println("Водим координаты необходимого поля.\n" +
-                "Введите координаты x1 и y1 :");
-
-        Point point1 = new Point(scanner2.nextInt(), scanner2.nextInt());
-
-        System.out.println("Введите координаты x2 и y2:");
-
-        Point point2 = new Point(scanner2.nextInt(), scanner2.nextInt());
+        Set<Point> valuesInSquare = new HashSet<>();
 
 
-        if (point1.getX() >= point2.getX() || point1.getY() >= point2.getY()) {
-            System.out.println("Некорректный ввод. Введите корректное значение");
-            inputValue(valuesInSquare);
+        try {
+            System.out.println("Вводим координаты необходимого поля.\n" +
+                    "Введите координаты x1 и y1 :");
 
-        } else if (point2.getX() - point1.getX() == 1) {
-            System.out.println("Некорректный ввод. Введите корректное значение.");
-            inputValue(valuesInSquare);
+            Point point1 = new Point(scanner2.nextInt(), scanner2.nextInt());
 
-        } else if (point1.getX() < 0 || point1.getY() < 0
-                || point2.getX() < 0 || point2.getY() < 0) {
-            System.out.println("Некорректный ввод. Введите корректное значение.");
-            inputValue(valuesInSquare);
+            System.out.println("Введите координаты x2 и y2:");
 
-        } else if (point1.getX() > 98 || point1.getY() > 98
-                || point2.getX() > 100 || point2.getY() > 100) {
-            System.out.println("Некорректный ввод. Введите корректное значение.");
-            inputValue(valuesInSquare);
-        }
+            Point point2 = new Point(scanner2.nextInt(), scanner2.nextInt());
 
 
-        for (int i = point1.getX() + 1; i < point2.getX(); i++) {
-            for (int j = point1.getY() + 1; j < point2.getY(); j++) {
+            if (point1.getX() >= point2.getX() || point1.getY() >= point2.getY()) {
+                throw new IOException("Некорректный ввод.");
 
-                valuesInSquare.add(new Point(i, j));
+            } else if (point2.getX() - point1.getX() == 1) {
+                throw new IOException("Некорректный ввод.");
+
+            } else if (point1.getX() < 0 || point1.getY() < 0
+                    || point2.getX() < 0 || point2.getY() < 0) {
+                throw new IOException("Некорректный ввод.");
+
+            } else if (point1.getX() > 98 || point1.getY() > 98
+                    || point2.getX() > 100 || point2.getY() > 100) {
+                throw new IOException("Некорректный ввод.");
             }
+
+            for (int i = point1.getX() + 1; i < point2.getX(); i++) {
+                for (int j = point1.getY() + 1; j < point2.getY(); j++) {
+
+                    valuesInSquare.add(new Point(i, j));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            getValueRequiredField();
         }
+        return valuesInSquare;
     }
+
 }
 
